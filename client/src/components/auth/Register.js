660c1c9f9098223403acc04e0c2c16f5830a306e
farooq023 <farooqtariq526@gmail.com>
2022-04-17 08:19:36 +0500

    Registration Page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -37,22 +37,49 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
     <div style={{ height:"100%", width:"100%", position:"absolute", flexDirection:"row", display:"flex", backgroundColor:"#F0F2F5"}}>
       <div style={{marginTop:"12%", marginLeft:"13%", height:"25%", width:"35%"}}>
         <h1 style={{color:"#1877F2"}}>Web Offensive Security</h1>
-        {/* <text style={{fontSize:'125%'}}>Web Offensive Security helps you in finding out weakpoints </text> */}
-        <text style={{fontSize:'125%'}}>.........</text>
+        <text style={{fontSize:'125%'}}>Web Offensive Security is useful for penetration testers and bug bounty hunters as it helps in finding out weakpoints in any Web System and its Security Controls.</text>
       </div>
-      <div style={{marginTop:"12%", marginLeft:"6%", height:"50%", width:"25%", border:"2.5px solid #1877F2", borderRadius:"5%", display:"flex", flexDirection:"column", alignItems:"center"}}>
-        <h1 style={{color:"#1877F2"}}>Sign Up</h1>
-        <text style={{fontSize:'125%'}}>Become An Agent</text>
-        
-        <div className="form-group">
+      <div style={{marginTop:"12%", marginLeft:"6%", height:"60%", width:"25%", border:"2.5px solid #1877F2", borderRadius:"5%", display:"flex", flexDirection:"column", alignItems:"center"}}>
+        <h1 style={{color:"#1877F2", marginTop:"8%"}}>Sign Up</h1>
+        <text style={{fontSize:'125%'}}>Become An Agent Now!</text>
+        <form style={{display:"flex", flexDirection:"column", alignItems:"center"}} onSubmit={onSubmit}>
           <input
+            style={{border:"2px solid #1877F2", height:"40%", width:"180%", marginTop:"10%"}}
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
+          <input
+            style={{border:"2px solid #1877F2", height:"20%", width:"180%", marginTop:"10%"}}
             type="email"
             placeholder="Email Address"
             name="email"
             value={email}
             onChange={onChange}
           />
-        </div>
+          <input
+            style={{border:"2px solid #1877F2", height:"20%", width:"180%", marginTop:"10%"}}
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+          <input
+            style={{border:"2px solid #1877F2", height:"20%", width:"180%", marginTop:"10%", marginBottom:"25%"}}
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+          />
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
       </div>
     </div>
   );
